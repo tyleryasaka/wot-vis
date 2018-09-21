8607b7de94c4f0fb66562a5782e9168bc2a15407
Tyler Yasaka <contact@tyleryasaka.me>
2018-09-21 08:40:25 -0700

    Create flexbox grid

--- src/App.js
+++ src/App.js
@@ -38,12 +38,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
-          <Graph graph={graphVis} options={options} events={events} />
+        <div className="container">
+          <div className="controls section">
+            <div className="section">
+            </div>
+          </div>
+          <div className="display section">
+            <Graph graph={graphVis} options={options} events={events} />
+          </div>
         </div>
       </div>
     );
