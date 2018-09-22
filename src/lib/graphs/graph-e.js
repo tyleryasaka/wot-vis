c4f82befbc1662873e03e8de2176084ac06b686c
Tyler Yasaka <contact@tyleryasaka.me>
2018-09-21 17:05:25 -0700

    Color graph e

--- src/lib/graphs/graph-e.js
+++ src/lib/graphs/graph-e.js
@@ -1,7 +1,10 @@
-const graphlib = require("@dagrejs/graphlib")
+const _ = require('underscore')
+const graphlib = require('@dagrejs/graphlib')
 
 function graphE() {
   let graph = new graphlib.Graph()
+  const confusedNodes = [10]
+  const badNodes = _.range(14, 27).concat('C')
   const doubleEdges = [
     [1, 2],
     [2, 'A'],
@@ -18,31 +21,31 @@ function graphE() {
 
     [8, 9],
     [9, 'B'],
-    ['B', 14],
+    ['B', 13],
 
     [10, 8],
     [8, 11],
     [9, 11],
     ['B', 11],
-    [14, 11],
+    [13, 11],
 
     [4, 11]
   ]
   const edges = [
-    [10, 13],
+    [10, 14],
 
-    [13, 15],
-    [13, 16],
-    [13, 17],
-    [13, 18],
-    [13, 19],
-    [13, 20],
-    [13, 21],
-    [13, 22],
-    [13, 23],
-    [13, 24],
-    [13, 25],
-    [13, 26],
+    [14, 15],
+    [14, 16],
+    [14, 17],
+    [14, 18],
+    [14, 19],
+    [14, 20],
+    [14, 21],
+    [14, 22],
+    [14, 23],
+    [14, 24],
+    [14, 25],
+    [14, 26],
 
     [15, 'C'],
     [16, 'C'],
@@ -63,6 +66,12 @@ function graphE() {
   })
   edges.forEach(([a, b]) => {
     graph.setEdge(String(a), String(b))
+  })
+  confusedNodes.forEach(node => {
+    graph.setNode(node, 'confused')
+  })
+  badNodes.forEach(node => {
+    graph.setNode(node, 'bad')
   })
   return graph
 }
